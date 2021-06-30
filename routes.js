cf76f20f1759a18c6f6bc67445961a367e90c2f9
nazirovdev <nazirovdev@github.com>
2021-06-30 19:03:57 +0700

    refactoring routing server :hammer:

---
+++ routes.js
@@ -0,0 +1,11 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World'
+        }
+    }
+];
+
+module.exports = routes;
